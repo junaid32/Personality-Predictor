c1922ccf6d5fc2010e885a01aae1475e333a1ff0
junaid32 <junaidjamshaid323@gmail.com>
2017-01-15 19:25:45 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict';
 var logger  = require('winston'),
-   port    = 3000;
+   port    = 8080;
 //require('cf-deployment-tracker-client').track();
 var server = require('./app');
 server.listen(port, function () {
